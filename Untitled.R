9a7343dc556857114c0c6050508ca3a3da6dafe9
jonellevillar <jonellevillar@gmail.com>
2024-06-16 12:49:04 +0200

    update

---
+++ Untitled.R
@@ -0,0 +1,5 @@
+-   Median study duration 6 weeks, maximum 13 weeks (PMID:37159349)
+-   Risperidone highest TDR and shortest Time to discontinuation (TTD) (PMID: 35770081)
+
+
+Leucht et al.(2013) **World Psychiatry**; Zhang et al.(2022) **Frontiers in Pharmacology**, 2022
